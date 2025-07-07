f572f8c28a9e998af5e96d4590514212c336f829
Alena Ghinna <109030617+huamulan13@users.noreply.github.com>
2025-07-07 18:59:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -134,7 +134,7 @@ if st.button("Analisis Sekarang"):
         with st.expander("Lihat Detail Proses"):
             st.write(f"**Teks Setelah Preprocessing:** `{processed_text}`")
             st.write(f"**Skor AFINN:** `{afinn_score}`")
-            st.write(f"**Probabilitas Prediksi (Negatif, Positif):** `{prediction_probs[0]}`")
+            #st.write(f"**Probabilitas Prediksi (Negatif, Positif):** `{prediction_probs[0]}`")
 
     else:
         st.warning("Harap masukkan teks untuk dianalisis.")
